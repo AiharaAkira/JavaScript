18421825f2f601dae8ab8bc0b9807d4cd29aa7ed
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2021-10-01 19:17:37 +0900

    ChromeWeb

--- ChromeWebProject/JS/background.js
+++ ChromeWebProject/JS/background.js
@@ -1,11 +1,27 @@
-const images = [
-    "1.jpg", "2.jpg", "3.jpg"
-];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//bg.js 
+ 
+const body = document.querySelector("body");
+ 
+const IMG_NUMBER = 3;
+ 
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `img/${imgNumber + 1}.jpg`; // 가져올 image경로 지정
+  image.classList.add("bgImage"); // image에 bgImage 클래스 추가 
+  body.appendChild(image); // body의 자식에 image추가
+}
+ 
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+ 
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+ 
+init();
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
--- ChromeWebProject/JS/weather.js
+++ ChromeWebProject/JS/weather.js
@@ -3,11 +3,21 @@ const API_KEY = "aa7b9386aea6402f6b5f06d2db012668";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-   
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
 }
+//aa7b9386aea6402f6b5f06d2db012668
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+//api.openweathermap.org/data/2.5/weather?lat=37.7533672&lon=127.1169957&appid=aa7b9386aea6402f6b5f06d2db012668&units=metric
+
 
 
 function onGeoError(){
