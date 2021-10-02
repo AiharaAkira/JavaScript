b909484034d5836aa4b03daa32c4ea0e4ed5b937
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2021-10-02 14:50:31 +0900

    css
    update

--- ChromeWebProject/JS/quotes.js
+++ ChromeWebProject/JS/quotes.js
@@ -30,8 +30,8 @@ const quotes = [{
     author: "比企谷八幡",
 }];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const quote = document.querySelector("#quote span:last-child");
+const author = document.querySelector("#quote span:first-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
